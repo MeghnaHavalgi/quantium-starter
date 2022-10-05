23ac9d3b4219d099d88f80955be97de8364e9c44
MeghnaHavalgi <meghnahavalgi@gmail.com>
2022-10-05 22:49:13 +0530

    modified

--- data/SoulFoods.py
+++ data/SoulFoods.py
@@ -1,5 +1,7 @@
-from dash import Dash, html, dcc
+import colorsys
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 def main():
@@ -32,20 +34,89 @@ def main():
     df2.to_csv("C:\\Users\\Akshay bruh\\Desktop\\quantium-starter\\data\\final_csv.csv", encoding='utf-8', index=False)
 # #creating a dash layout
     app = Dash(__name__)
-    fig = px.line(df2, x="date", y="sales")
-    app.layout = html.Div(children=[
-        html.H1(children='SoulFoods'),
-        html.Div(children='''Pink morsel sales over time.'''),
+
+    colors = {
+    'background': 'pink',
+    'text': '#7FDBFF'
+    }
+
+    # fig = px.line(df2, x="date", y="sales", color="region")
+
+    app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+        html.H1(
+            children='SoulFoods',
+            style={
+              'textAlign': 'center',
+              'color': colors['text']
+              },
+        ),
+        html.Div(children='''Pink morsel sales over time.''',  style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }),
+
+        html.Div([
+            html.Br(),
+            html.Label(['Region:'], style={'font-weight': 'bold'}),
+            dcc.RadioItems(
+                id='xradioitem',
+                options=[
+                    {'label': 'All', 'value':'All'},
+                    # {'label': 'north', 'value':'north'},
+                    # {'label': 'south', 'value':'south'},
+                    # {'label': 'east', 'value':'east'},
+                    # {'label': 'west', 'value':'west'},
+
+                ],
+                value = 'All',
+                style={"width":"50%"}
+
+            ),
+        ]),
+
+        html.Div([
+            html.Br(),
+            html.Label(['Region:'], style={'font-weight': 'bold'}),
+            dcc.RadioItems(
+                id='yradioitem',
+                options=[
+                 
+                    {'label': 'north', 'value':'north'},
+                    {'label': 'south', 'value':'south'},
+                    {'label': 'east', 'value':'east'},
+                    {'label': 'west', 'value':'west'},
+
+                ],
+                # value = 'north',
+                style={"width":"50%"}
+
+            ),
+
+        ]),
 
         dcc.Graph(
-            id='example-graph',
-            figure=fig
+        id='example-graph',
+        # figure=fig
         )
-
     ])
 
+    @app.callback(
+        Output(component_id = 'example-graph', component_property='figure'),
+        [Input(component_id= 'xradioitem', component_property='value'),
+         Input(component_id= 'yradioitem', component_property='value')]
+        )
 
+    def update_graph(x_axis, y_axis):
+        dff=df2
+        barchart = px.bar(
+            data_frame=dff,
+            x=dff['date'],
+            y=dff['sales'],
+            color='region',
+        )
+        return barchart
     
+
     app.run_server(debug=True)
 
 if __name__ == "__main__":
